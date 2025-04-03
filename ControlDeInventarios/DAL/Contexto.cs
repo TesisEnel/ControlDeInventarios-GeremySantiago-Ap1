42eeb36ff297594a023bc7a8ec3881ec09dbdf8c
Geremy De los Santos S. <geremiorlando28@hotmail.com>
2025-04-03 10:14:10 -0400

    Creacion del Contexto

---
+++ ControlDeInventarios/DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using ControlDeInventarios.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControlDeInventarios.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Usuario> Usuario { get; set; }
+    public DbSet<Productos> Productos { get; set; }
+    public DbSet<Categorias> Categorias { get; set; }
+    public DbSet<EntradaProductos> Entradas { get; set; }
+    public DbSet<SalidaProductos> Salidas { get; set; }
+}
