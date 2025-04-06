cc1e6a35f7f2ac7b059ea9cd358af84f1c210046
Geremy De los Santos S. <geremiorlando28@hotmail.com>
2025-04-05 23:31:20 -0400

    Modificaciones

--- ControlDeInventarios/Program.cs
+++ ControlDeInventarios/Program.cs
@@ -1,12 +1,47 @@
 using ControlDeInventarios.Components;
+using ControlDeInventarios.DAL;
+using ControlDeInventarios.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Agrega autenticación con cookies
+builder.Services.AddAuthentication("Cookies")
+    .AddCookie("Cookies", options =>
+    {
+        options.LoginPath = "/Autenticacion/Login";
+        options.AccessDeniedPath = "/Autenticacion/Denegado";
+    });
+builder.Services.AddAuthorization();
+
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+//Obtenemos el ConStr para usarlo en el contexto
+var ConStr = builder.Configuration.GetConnectionString("DefaultConnection");
+
+//Agregamos el contexto al builder con el ConStr
+builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConStr));
+
+builder.Services.AddScoped<UsuarioService>();
+builder.Services.AddScoped<ProductosService>();
+builder.Services.AddScoped<CategoriaService>();
+builder.Services.AddScoped<EntradaService>();
+builder.Services.AddScoped<SalidaService>();
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<CustomAuthProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthProvider>();
+builder.Services.AddAuthorizationCore(); // Necesario para [Authorize]
+
+
 var app = builder.Build();
+
+// Habilita autenticación y autorización
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
